5d3b7ce5b6d39a15378482accc9c177323496680
Fernando Nicolás <fersanti2896@gmail.com>
2022-11-03 02:36:54 -0600

    Creando proyecto inicial y configurando el DbContext

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManejoTareas {
+    public class ApplicationDbContext : DbContext {
+        public ApplicationDbContext(DbContextOptions options) : base(options) {
+        }
+    }
+}
